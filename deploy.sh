b7dfbcfbb3c3f32803a9bda8cdfca1c4f09cd7ee
olivierayache <olivier.ayache@gmail.com>
2018-01-16 00:31:18 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl --header 'X-Api-Key: '$CLOUDSMITH_API_TOKEN'' -X GET https://api-prd.cloudsmith.io/v1/user/self/
+echo "Hello"
+#curl --header 'X-Api-Key: '$CLOUDSMITH_API_TOKEN'' -X GET https://api-prd.cloudsmith.io/v1/user/self/
